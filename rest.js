bc5e27010a02fbb5ab7c7055734957d614b47f0c
Joon Park <parkjoon@umich.edu>
2017-02-06 18:22:48 -0500

    General work.

--- rest.js
+++ rest.js
@@ -5,24 +5,30 @@ function REST_ROUTER(router, connection, sha512) {
 	self.handleRoutes(router, connection, sha512);
 }
 
-REST_ROUTER.prototype.handleRoutes= function(router, connection, sha512) {
+REST_ROUTER.prototype.handleRoutes = function(router, connection, sha512) {
 	router.get('/', function(req, res) {
-		res.json({'Message' : 'Hello World !'});
-	})
+		res.json({ 'Message': 'Hello World !'});
+	});
 
-	router.get('/jobs',function(req, res) {
-        var query = 'SELECT * FROM ??';
-        var table = ['job_posts'];
-        query = mysql.format(query, table);
-        connection.query(query, function(err,rows){
-            if(err) {
-                res.json({'Error' : true, 'Message' : 'Error executing MySQL query'});
-            }
+	/* Freelancers */
+	router.get('/freelancers', function(req, res) {
+		return { freelancerProfiles: [] };
+	});
+
+	/* Jobs */
+	router.get('/jobs', function(req, res) {
+		var query = 'SELECT * FROM ??';
+		var table = ['job_posts'];
+		query = mysql.format(query, table);
+		connection.query(query, function(err, rows) {
+			if(err) {
+				res.json({'Error': true, 'Message': 'Error executing MySQL query'});
+			}
 			else {
-                res.json({'Error' : false, 'Message' : 'Success', 'jobs' : rows});
-            }
-        });
-    });
-}
+				res.json({'Error': false, 'Message': 'Success', 'jobs': rows});
+			}
+		});
+	});
+};
 
 module.exports = REST_ROUTER;
